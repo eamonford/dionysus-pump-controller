2a33a5cee9f1111fc6e5762eae1976f4fd991684
Eamon Ford <eamonford@gmail.com>
2017-03-17 17:33:28 -0500

    chore(ValveController): move code to new valve controller class

--- src/pump-controller.ino
+++ src/pump-controller.ino
@@ -2,11 +2,13 @@
 #include <SparkJson.h>
 #include "Constants.h"
 #include <vector>
+#include "ValveController.h"
 
 using namespace std;
 
 int PUMP = A3;
 ProtocolController* protocolController;
+ValveController* valveController;
 
 void activatePump() {
     analogWrite(PUMP, 4095);
@@ -14,53 +16,6 @@ void activatePump() {
 
 void deactivatePump() {
     analogWrite(PUMP, 0);
-}
-
-bool openValveWithId(int valveId) {
-    Datagram* request = new Datagram(valveId, OPEN_VALVE, NOOP);
-    if (!protocolController->sendDatagram(request, MASTER))
-      return false;
-    Datagram* response = protocolController->getDatagram();
-    bool success = response->command == OPEN_VALVE;
-    delete response;
-    return success;
-}
-
-bool closeValveWithId(int valveId) {
-    Datagram* request = new Datagram(valveId, CLOSE_VALVE, NOOP);
-    if (!protocolController->sendDatagram(request, MASTER))
-      return false;
-    Datagram* response = protocolController->getDatagram();
-    bool success = response->command == CLOSE_VALVE;
-    delete response;
-    return success;
-}
-
-int execute(String json) {
-    StaticJsonBuffer<250> jsonBuffer;
-    JsonObject& root = jsonBuffer.parseObject((char*)json.c_str());
-    JsonArray& valves = root["i"];
-    int totalDuration = 0;
-    for (int i = 0; i < valves.size(); i++) {
-        int valveId = valves[i]["id"];
-        int duration = valves[i]["d"];
-        if (openValveWithId(valveId)) {
-          totalDuration += duration;
-          activatePump();
-          delay(duration*1000);
-          deactivatePump();
-          if (!closeValveWithId(valveId))
-            break;
-        }
-    }
-    return totalDuration;
- }
-
-bool assignValveId(int id) {
-  Datagram* datagram = new Datagram(FIRST_UNIDENTIFIED, SET_ID, id);
-  protocolController->sendDatagram(datagram, MASTER);
-  Datagram* response = protocolController->getDatagram();
-  return response->command == SET_ID && response->arg == id;
 }
 
 String generateJsonForIds(vector<int>* ids) {
@@ -72,41 +27,37 @@ String generateJsonForIds(vector<int>* ids) {
   return idsString;
 }
 
-void identifyAllSlaves() {
-    Datagram* msg = new Datagram(EVERYONE, IDENTIFY, NOOP);
-    protocolController->sendDatagram(msg, MASTER);
-    delete msg;
-
-    vector<int>* valveIds = new vector<int>();
-    Datagram* response;
-    while ((response = protocolController->getDatagram())->command != END_OF_CHAIN) {
-      // if a valve is unidentified, mark it as needing to be assigned an ID
-      if (response->command == IDENTIFY)
-        valveIds->push_back(response->arg);
-      delete response;
+int execute(String json) {
+    StaticJsonBuffer<250> jsonBuffer;
+    JsonObject& root = jsonBuffer.parseObject((char*)json.c_str());
+    JsonArray& valves = root["i"];
+    int totalDuration = 0;
+    for (int i = 0; i < valves.size(); i++) {
+        int valveId = valves[i]["id"];
+        int duration = valves[i]["d"];
+        if (valveController->openValveWithId(valveId)) {
+          totalDuration += duration;
+          activatePump();
+          delay(duration*1000);
+          deactivatePump();
+          if (!valveController->closeValveWithId(valveId))
+            break;
+        }
     }
-    delete response;
-
-    for (int i = 0; i < valveIds->size(); i++) {
-      if ((*valveIds)[i] == 0) {
-        int newId = rand() % 255;
-        if (assignValveId(newId))
-          (*valveIds)[i] = newId;
-      }
-    }
-    Particle.publish("valves", generateJsonForIds(valveIds));
-    delete valveIds;
-}
+    return totalDuration;
+ }
 
 void setup() {
     Particle.function("execute", execute);
-
-    protocolController = new ProtocolController(&Serial1);
     pinMode(PUMP, OUTPUT);
     Serial.begin(9600);
     Serial1.begin(9600);
 
-    identifyAllSlaves();
+    protocolController = new ProtocolController(&Serial1);
+    valveController = new ValveController(protocolController);
+
+    vector<int>* valveIds = valveController->identifyAllSlaves();
+    Particle.publish("valves", generateJsonForIds(valveIds));
 }
 
 void loop() {
